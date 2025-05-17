ae18f59a7c32117d63619be722541a81abe0d58b
Kouhei.Sano <apple.x.co@gmail.com>
2025-05-17 16:30:04 +0900

    Add generated Cargo.lock file to repository
    This commit introduces the `Cargo.lock` file, which is automatically generated by Cargo to ensure reproducible builds. It captures exact versions of dependencies for consistent compilation across different environments.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # rs-sendmail
+
+Config
+
+```json
+{
+  "$schema": "./schema/schema.json",
+  "from": {
+    "user": "hello",
+    "domain": "example.com",
+    "name": "HELLO"
+  },
+  "to": [
+    {
+      "user": "hello",
+      "domain": "example.com"
+    }
+  ],
+  "subject": "This mail is TEST",
+  "body": "HI!"
+}
+```
+
+Run
+
+```bash
+SMTP_HOST=example.com SMTP_PASSWORD=xxx SMTP_PORT=587 SMTP_USER=xxx rs-sendmail --config config.json 
+```
---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use anyhow::{Result, anyhow};
+use clap::Parser;
+use lettre::Transport;
+use serde::Deserialize;
+use std::fs::read_to_string;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    config: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Destination {
+    user: String,
+    domain: String,
+    name: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    from: Destination,
+    to: Vec<Destination>,
+    cc: Option<Vec<Destination>>,
+    bcc: Option<Vec<Destination>>,
+    subject: String,
+    body: String,
+    html: Option<String>,
+}
+
+const JSON_SCHEMA_BYTES: &'static [u8] = include_bytes!("../schema/schema.json");
+
+fn main() -> Result<()> {
+    let smtp_host = std::env::var("SMTP_HOST")?;
+    let smtp_port = std::env::var("SMTP_PORT").unwrap_or("25".to_string()); // NOTE: 25,465,587
+    let smtp_encryption = std::env::var("SMTP_ENCRYPTION").unwrap_or("none".to_string()); // NOTE: none,tls,starttls
+    let smtp_user = std::env::var("SMTP_USER")?;
+    let smtp_password = std::env::var("SMTP_PASSWORD")?;
+
+    let args = Args::parse();
+
+    let schema = serde_json::from_slice(JSON_SCHEMA_BYTES)?;
+    let validator = jsonschema::validator_for(&schema)?;
+    let json_string = read_to_string(args.config)?;
+    let json: serde_json::Value = serde_json::from_str(&json_string)?;
+
+    if !validator.validate(&json).is_ok() {
+        return Err(anyhow!("JSON validation failed".to_string()));
+    }
+
+    let config: Config = serde_json::from_str(&json_string)?;
+
+    let mut message_builder = lettre::Message::builder();
+    message_builder = message_builder.from(lettre::message::Mailbox::new(
+        config.from.name.clone(),
+        lettre::Address::new(config.from.user.clone(), config.from.domain.clone())?,
+    ));
+    message_builder = message_builder.reply_to(lettre::message::Mailbox::new(
+        config.from.name.clone(),
+        lettre::Address::new(config.from.user.clone(), config.from.domain.clone())?,
+    ));
+    for to in config.to {
+        message_builder = message_builder.to(lettre::message::Mailbox::new(
+            to.name.clone(),
+            lettre::Address::new(to.user.clone(), to.domain.clone())?,
+        ))
+    }
+    if let Some(cc) = &config.cc {
+        for cc in cc {
+            message_builder = message_builder.cc(lettre::message::Mailbox::new(
+                cc.name.clone(),
+                lettre::Address::new(cc.user.clone(), cc.domain.clone())?,
+            ))
+        }
+    }
+    if let Some(bcc) = &config.bcc {
+        for bcc in bcc {
+            message_builder = message_builder.bcc(lettre::message::Mailbox::new(
+                bcc.name.clone(),
+                lettre::Address::new(bcc.user.clone(), bcc.domain.clone())?,
+            ))
+        }
+    }
+    message_builder = message_builder.subject(config.subject.clone());
+    message_builder = message_builder.header(lettre::message::header::ContentType::TEXT_PLAIN);
+
+    // TODO: Attachments, HTML
+    let message = message_builder.body(config.body)?;
+
+    let creds = lettre::transport::smtp::authentication::Credentials::new(smtp_user, smtp_password);
+    let mailer = match smtp_encryption.as_str() {
+        "starttls" => lettre::SmtpTransport::starttls_relay(&smtp_host)?
+            .port(smtp_port.parse::<u16>()?)
+            .credentials(creds)
+            .build(),
+        _ => lettre::SmtpTransport::relay(&smtp_host)?
+            .port(smtp_port.parse::<u16>()?)
+            .credentials(creds)
+            .build(),
+    };
+
+    match mailer.send(&message) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => eprintln!("Could not send email: {:?}", e),
+    };
+
+    Ok(())
+}
